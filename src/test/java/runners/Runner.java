78aab0d95614a51c2075018fe5f7cc5388f7b77f
tvelihan <velihanturk69@gmail.com>
2023-05-27 19:27:38 +0300

    27.05.2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,9 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin={"html:target\\cucumber-reports.html"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@regression and @smoke",
+        tags = "@ss",
 
         dryRun = false
 )
